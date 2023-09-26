dfba09c6a50f49f95c5d57212046f343704f9dc4
Mathesh <144098846+mathesh-me@users.noreply.github.com>
2023-09-26 23:57:00 +0530

    Create bucket-policy.tf

---
+++ Static Web Hosting Terraform Configuration files/bucket-policy.tf
@@ -0,0 +1,19 @@
+resource "aws_s3_bucket_policy" "public_read_access" {
+  bucket = aws_s3_bucket.bucket1.id
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+	  "Principal": "*",
+      "Action": [ "s3:GetObject" ],
+      "Resource": [
+        "${aws_s3_bucket.bucket1.arn}",
+        "${aws_s3_bucket.bucket1.arn}/*"
+      ]
+    }
+  ]
+}
+EOF
+}
