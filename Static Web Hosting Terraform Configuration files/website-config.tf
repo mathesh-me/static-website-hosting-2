e2a1de5e5c6654487be1953bd4518d48ca930f1a
Mathesh <144098846+mathesh-me@users.noreply.github.com>
2023-09-26 23:56:15 +0530

    Create website-config.tf

---
+++ Static Web Hosting Terraform Configuration files/website-config.tf
@@ -0,0 +1,12 @@
+resource "aws_s3_bucket_website_configuration" "bucket1" {
+  bucket = aws_s3_bucket.bucket1.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+
+}
